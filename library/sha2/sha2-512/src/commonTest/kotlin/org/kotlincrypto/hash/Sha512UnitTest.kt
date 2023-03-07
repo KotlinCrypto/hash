c6fd67a7ad3bf03be539d1a4b3ae314ef70c0d29
Matthew Nelson <developer@matthewnelson.io>
2023-03-06 19:29:43 -0500

    Update `kotlincrypto.core` to `0.1.1` (#7)

--- library/sha2/sha2-512/src/commonTest/kotlin/org/kotlincrypto/hash/Sha512UnitTest.kt
+++ library/sha2/sha2-512/src/commonTest/kotlin/org/kotlincrypto/hash/Sha512UnitTest.kt
@@ -22,7 +22,7 @@ open class Sha512UnitTest: DigestUnitTest() {
     override val digest: Digest = Sha512()
     final override val expectedResetHash: String = "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
     final override val expectedUpdateSmallHash: String = "6f23b0a36ea92b792dc6b19739ea4b9ee565478e3107016bf7749898b963b1247cdccf39f63f97703a001e2f97a859d31f39d5c277e0594ad06677242ed93fd8"
-    final override val expectedUpdateMediumHash: String = "c4e474c33b13b1cf52989dbb31b0d307554d44314729ab2525854c36484914d3153b573887e55f485406df3ab45c7582fcbfb32842defea701189a0210d7184c"
+    final override val expectedUpdateMediumHash: String = "47689826e2d98e51e325c1b66723fa08fde6e894b8fe1bc7f1ae2860d9c709776f77bfca856a0688c29a275c72d8738d6afc62e808ac8f01ef29a29381078719"
 
     @Test
     override fun givenDigest_whenReset_thenDigestDigestReturnsExpected() {
