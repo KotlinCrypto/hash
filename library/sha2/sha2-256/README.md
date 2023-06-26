823e2ae249d772253723dc7992308c5ea0757987
Matthew Nelson <developer@matthewnelson.io>
2023-06-26 07:43:19 -0400

    Remove sha2-256 and sha2-512 modules

--- library/sha2/sha2-256/README.md
+++
@@ -1,4 +0,0 @@
-# sha2-256
-
-Old module contents were combined into `sha2`. This publication is solely to mitigate breaking 
-others' projects and inform them they should update their dependency.
--- tools/check-publication/build.gradle.kts
+++ tools/check-publication/build.gradle.kts
@@ -44,8 +44,6 @@ kmpConfiguration {
                     implementation("$group:md5")
                     implementation("$group:sha1")
                     implementation("$group:sha2")
-                    implementation("$group:sha2-256")
-                    implementation("$group:sha2-512")
                     implementation("$group:sha3")
                 }
             }
