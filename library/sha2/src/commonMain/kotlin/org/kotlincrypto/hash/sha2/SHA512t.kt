c9f1ff110b66bf229d0bb80572b1dc9aca824447
Matthew Nelson <developer@matthewnelson.io>
2024-12-20 22:06:26 -0500

    Return early when byte array is full

--- library/sha2/src/commonMain/kotlin/org/kotlincrypto/hash/sha2/SHA512t.kt
+++ library/sha2/src/commonMain/kotlin/org/kotlincrypto/hash/sha2/SHA512t.kt
@@ -111,78 +111,79 @@ public class SHA512t: Bit64Digest {
             return ByteArray(0)
         }
 
-        val out = ByteArray(digestLength())
-        var i = 0
+        val len = digestLength()
+        val out = ByteArray(len)
 
-        fun Long.putOut() {
-            if (i == out.size) return
+        var i = 0
+        fun Long.putOut(): Unit? {
             out[i++] = toByte()
+            return if (i == len) null else Unit
         }
 
-        (a shr 56).putOut()
-        (a shr 48).putOut()
-        (a shr 40).putOut()
-        (a shr 32).putOut()
-        (a shr 24).putOut()
-        (a shr 16).putOut()
-        (a shr  8).putOut()
-        (a       ).putOut()
-        (b shr 56).putOut()
-        (b shr 48).putOut()
-        (b shr 40).putOut()
-        (b shr 32).putOut()
-        (b shr 24).putOut()
-        (b shr 16).putOut()
-        (b shr  8).putOut()
-        (b       ).putOut()
-        (c shr 56).putOut()
-        (c shr 48).putOut()
-        (c shr 40).putOut()
-        (c shr 32).putOut()
-        (c shr 24).putOut()
-        (c shr 16).putOut()
-        (c shr  8).putOut()
-        (c       ).putOut()
-        (d shr 56).putOut()
-        (d shr 48).putOut()
-        (d shr 40).putOut()
-        (d shr 32).putOut()
-        (d shr 24).putOut()
-        (d shr 16).putOut()
-        (d shr  8).putOut()
-        (d       ).putOut()
-        (e shr 56).putOut()
-        (e shr 48).putOut()
-        (e shr 40).putOut()
-        (e shr 32).putOut()
-        (e shr 24).putOut()
-        (e shr 16).putOut()
-        (e shr  8).putOut()
-        (e       ).putOut()
-        (f shr 56).putOut()
-        (f shr 48).putOut()
-        (f shr 40).putOut()
-        (f shr 32).putOut()
-        (f shr 24).putOut()
-        (f shr 16).putOut()
-        (f shr  8).putOut()
-        (f       ).putOut()
-        (g shr 56).putOut()
-        (g shr 48).putOut()
-        (g shr 40).putOut()
-        (g shr 32).putOut()
-        (g shr 24).putOut()
-        (g shr 16).putOut()
-        (g shr  8).putOut()
-        (g       ).putOut()
-        (h shr 56).putOut()
-        (h shr 48).putOut()
-        (h shr 40).putOut()
-        (h shr 32).putOut()
-        (h shr 24).putOut()
-        (h shr 16).putOut()
-        (h shr  8).putOut()
-        (h       ).putOut()
+        (a shr 56).putOut() ?: return out
+        (a shr 48).putOut() ?: return out
+        (a shr 40).putOut() ?: return out
+        (a shr 32).putOut() ?: return out
+        (a shr 24).putOut() ?: return out
+        (a shr 16).putOut() ?: return out
+        (a shr  8).putOut() ?: return out
+        (a       ).putOut() ?: return out
+        (b shr 56).putOut() ?: return out
+        (b shr 48).putOut() ?: return out
+        (b shr 40).putOut() ?: return out
+        (b shr 32).putOut() ?: return out
+        (b shr 24).putOut() ?: return out
+        (b shr 16).putOut() ?: return out
+        (b shr  8).putOut() ?: return out
+        (b       ).putOut() ?: return out
+        (c shr 56).putOut() ?: return out
+        (c shr 48).putOut() ?: return out
+        (c shr 40).putOut() ?: return out
+        (c shr 32).putOut() ?: return out
+        (c shr 24).putOut() ?: return out
+        (c shr 16).putOut() ?: return out
+        (c shr  8).putOut() ?: return out
+        (c       ).putOut() ?: return out
+        (d shr 56).putOut() ?: return out
+        (d shr 48).putOut() ?: return out
+        (d shr 40).putOut() ?: return out
+        (d shr 32).putOut() ?: return out
+        (d shr 24).putOut() ?: return out
+        (d shr 16).putOut() ?: return out
+        (d shr  8).putOut() ?: return out
+        (d       ).putOut() ?: return out
+        (e shr 56).putOut() ?: return out
+        (e shr 48).putOut() ?: return out
+        (e shr 40).putOut() ?: return out
+        (e shr 32).putOut() ?: return out
+        (e shr 24).putOut() ?: return out
+        (e shr 16).putOut() ?: return out
+        (e shr  8).putOut() ?: return out
+        (e       ).putOut() ?: return out
+        (f shr 56).putOut() ?: return out
+        (f shr 48).putOut() ?: return out
+        (f shr 40).putOut() ?: return out
+        (f shr 32).putOut() ?: return out
+        (f shr 24).putOut() ?: return out
+        (f shr 16).putOut() ?: return out
+        (f shr  8).putOut() ?: return out
+        (f       ).putOut() ?: return out
+        (g shr 56).putOut() ?: return out
+        (g shr 48).putOut() ?: return out
+        (g shr 40).putOut() ?: return out
+        (g shr 32).putOut() ?: return out
+        (g shr 24).putOut() ?: return out
+        (g shr 16).putOut() ?: return out
+        (g shr  8).putOut() ?: return out
+        (g       ).putOut() ?: return out
+        (h shr 56).putOut() ?: return out
+        (h shr 48).putOut() ?: return out
+        (h shr 40).putOut() ?: return out
+        (h shr 32).putOut() ?: return out
+        (h shr 24).putOut() ?: return out
+        (h shr 16).putOut() ?: return out
+        (h shr  8).putOut() ?: return out
+        (h       ).putOut() ?: return out
 
         return out
     }
