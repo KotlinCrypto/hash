59ce377efacd6f0e02101d46d655b36bec58f40d
Matthew Nelson <developer@matthewnelson.io>
2025-01-01 04:57:47 -0500

    WIP: Fixup build

--- library/blake2/src/jvmMain/java9/module-info.java
+++ library/blake2/src/jvmMain/java9/module-info.java
@@ -1,6 +1,7 @@
-@SuppressWarnings("JavaModuleNaming")
-module org.kotlincrypto.hash.blake2b {
+module org.kotlincrypto.hash.blake2 {
     requires kotlin.stdlib;
+    requires transitive org.kotlincrypto.core.digest;
+    requires org.kotlincrypto.endians;
 
     exports org.kotlincrypto.hash.blake2;
 }
