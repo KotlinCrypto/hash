57a53ada29ee336ca8973f386fd2e00da5587a38
Matthew Nelson <developer@matthewnelson.io>
2025-02-16 05:13:19 -0500

    Update benchmark: 0.4.11 -> 0.4.13

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -15,6 +15,7 @@
  **/
 import io.matthewnelson.kmp.configuration.extension.container.target.KmpTarget
 import kotlinx.benchmark.gradle.BenchmarksExtension
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 import org.jetbrains.kotlin.konan.target.HostManager
 import org.jetbrains.kotlin.konan.target.KonanTarget
@@ -41,6 +42,7 @@ kmpConfiguration {
         }
 
         js { target { browser(); nodejs() }; register() }
+        @OptIn(ExperimentalWasmDsl::class)
         wasmJs { target { browser(); nodejs() }; register() }
 
         val nativeHost = "nativeHost"
