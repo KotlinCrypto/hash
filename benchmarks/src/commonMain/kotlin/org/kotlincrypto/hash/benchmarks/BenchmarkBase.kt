c639dbd8aa954584af01e81b94e6f466e874bc4e
Matthew Nelson <developer@matthewnelson.io>
2025-02-16 06:04:47 -0500

    Change license header names to KotlinCrypto

--- benchmarks/src/commonMain/kotlin/org/kotlincrypto/hash/benchmarks/BenchmarkBase.kt
+++ benchmarks/src/commonMain/kotlin/org/kotlincrypto/hash/benchmarks/BenchmarkBase.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Matthew Nelson
+ * Copyright (c) 2024 KotlinCrypto
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
