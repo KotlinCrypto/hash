c1ec649b629d5371c5945c1ee42a0f401f82d653
Matthew Nelson <developer@matthewnelson.io>
2025-02-16 22:13:35 -0500

    Add test for appropriate t parameter

---
+++ library/sha2/src/commonTest/kotlin/org/kotlincrypto/hash/sha2/SHA512tUnitTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 KotlinCrypto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.kotlincrypto.hash.sha2
+
+import org.kotlincrypto.error.InvalidParameterException
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class SHA512tUnitTest {
+
+    @Test
+    fun givenSHA512tDigest_whenInvalidBitStrength_thenThrowsException() {
+        assertFailsWith<InvalidParameterException> { SHA512t(-1) }
+        assertFailsWith<InvalidParameterException> { SHA512t(384) }
+        assertFailsWith<InvalidParameterException> { SHA512t(512) }
+        assertFailsWith<InvalidParameterException> { SHA512t(512 - 8 + 1) }
+    }
+}
